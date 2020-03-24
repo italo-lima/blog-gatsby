283dda65e2bde8e9f4675e25061e20d037f87536
italo-lima <italojonas@hotmail.com>
2020-03-23 23:14:22 -0300

    initialized pagination

--- gatsby-node.js
+++ gatsby-node.js
@@ -17,25 +17,52 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
 exports.createPages = async ({ graphql, actions }) => {
     const { createPage } = actions
     const result = await graphql(`
-      query {
-        allMarkdownRemark {
+      query PostList {
+        allMarkdownRemark(sort: {order: DESC, fields: frontmatter___date}) {
           edges {
             node {
               fields {
                 slug
               }
+              timeToRead
+              frontmatter {
+                background
+                category
+                date(formatString: "DD [de] MMMM [de] YYYY", locale: "pt-br")
+                description
+                title
+              }
             }
           }
         }
-      }
-    `)
-    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      }`
+    )
+
+    const posts = result.data.allMarkdownRemark.edges
+
+    posts.forEach(({ node }) => {
       createPage({
         path: node.fields.slug,
         component: path.resolve(`./src/templates/blog-post.js`),
         context: {
           slug: node.fields.slug,
         },
+      })
+    })
+
+    const postsPerPage = 2
+    const numPages = Math.ceil(posts.length / postsPerPage)
+    
+    Array.from({length: numPages}).forEach((_, i) => {
+      createPage({
+        path: i == 0 ? '/' : `/page/${i+1}`,
+        component: path.resolve(`./src/templates/blog-list.js`),
+        context: {
+          limit: postsPerPage,
+          skip: i * postsPerPage,
+          numPages,
+          currentPage: i + 1
+        }
       })
     })
   }
